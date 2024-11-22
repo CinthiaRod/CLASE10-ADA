db2e153aa956f8d1e2de0590cf8a2b38bfeeafae
CinthiaRod <Cinthiarb28@gmail.com>
2024-11-22 14:57:57 -0800

    Código del ejercicio

--- Ejercicio2.js
+++ Ejercicio2.js
@@ -6,5 +6,13 @@
 //2.Pide al usuario que ingrese su edad.
 //3.Pide al usuario que ingrese su peso.
 //4.Muestra un mensaje personalizado que incluya el nombre, la edad y el peso del usuario.
-//Pista: En el ejercicio anterior, declaramos valores en las variables. Ahora nos toca pedir datos al usuario, por lo que usaremos prompt-sync para solicitar esta información.
+//Pista: En el ejercicio anterior, declaramos valores en las variables.
+// Ahora nos toca pedir datos al usuario, por lo que usaremos prompt-sync para solicitar esta información.
 
+const prompt = require('prompt-sync')();
+
+let Name= prompt("Enter your name:");
+let Age= parseInt(prompt("Enter your age:"));
+let Weight= parseFloat(prompt("Enter your weight:"))
+
+console.log( 'Hi', Name , 'your age is' , Age, 'and your weight is', Weight )
