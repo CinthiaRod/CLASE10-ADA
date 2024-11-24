13d78ac6e4d67cd8d4e32d48679b8289d57cb093
CinthiaRod <Cinthiarb28@gmail.com>
2024-11-23 22:13:57 -0800

    Código del Ejercicio

--- Ejercicio7.js
+++ Ejercicio7.js
@@ -6,3 +6,19 @@
 //(usando una variable temporal, aritmética o asignación simultanea), 
 //eres libre de elegir el que desees, eso si, investiga sobre el que usaras.
 
+const prompt = require ('prompt-sync')();
+
+let Value1 = 12;
+let Value2 = 8;
+
+console.log ('Before exchange variables');
+console.log ('First number:', Value1);
+console.log ('Second number:', Value2);
+
+let temporal= Value1;
+Value1 = Value2;
+Value2 = temporal;
+
+console.log ('After exchange variables');
+console.log ('First number:', Value1);
+console.log ('Second number:' , Value2);
